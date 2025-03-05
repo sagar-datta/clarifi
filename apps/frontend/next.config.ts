7bb50de320081390ace9387163b8b9d3fff47c7d
sagar-datta <s@gar.jp.net>
2025-03-05 20:50:46 +0800

    vercel deployment bug fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const config: NextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default config;
